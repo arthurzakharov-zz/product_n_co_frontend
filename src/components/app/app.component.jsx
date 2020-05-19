5b0ee843a64d5091f9c33453745e9bab43b4c92e
arthurzakharov <zakharov.arthur@hotmail.com>
2020-05-19 23:00:39 +0300

    add reset, add product component (basics), add prettier,

--- src/components/app/app.component.jsx
+++ src/components/app/app.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './app.module.css';
+import Product from '../product/product.component';
 
 function App() {
   return (
     <div className={styles.App}>
-    
+      <Product />
     </div>
   );
 }
